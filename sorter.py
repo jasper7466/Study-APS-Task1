a2e6b9acdc580ddb4bf4ca852325ef9a19ab1628
Tim <jasper7466@yandex.ru>
2020-05-01 19:37:23 +0500

    Файловая система, парсинг атрибутов. Реализован поиск, переименовывание и перемещение файлов - поиск на всю глубину вложенности директорий - переименовывание по заданному правилу - копирование в директорию назначения - вывод ошибки при пустом теге "исполнитель" или "альбом"

--- sorter.py
+++ sorter.py
@@ -2,14 +2,30 @@ import os
 import click
 import eyed3 as mp3
 
+mp3.log.setLevel("ERROR")   # Отключаем вывод предупреждений. Теперь в консоль будут выводиться только ошибки.
+
 
 @click.command()
 @click.option('-s', '--src_dir', default=os.getcwd(), help='Source directory')
 @click.option('-d', '--dst_dir', default=os.getcwd(), help='Destination directory')
-def sort(src_dir, dst_dir):
+@click.option('-r', '--nested', default=False, help='Set "True" for enable searching nested files inside subdirectories')
+def sort(src_dir, dst_dir, nested=False):
     """Simple program that sorts mp3-files."""
-    print(src_dir)
-    print(dst_dir)
+    n = 0
+    for dir, dirs, files in os.walk(src_dir):
+        for file in files:
+            if file.endswith('.mp3'):                       # Если mp3
+                f = mp3.load(os.path.join(dir, file))
+                if f.tag.album_artist and f.tag.album:      # Если прописаны теги "исполнитель" и "альбом"
+                    f.rename('rebase-in-progress')
+                    if f.tag.title:                         # Если прописан тег "название"
+                        name = f'{f.tag.title} - {f.tag.album_artist} - {f.tag.album}'
+                    else:
+                        name = f'{file[:-4]} - {f.tag.album_artist} - {f.tag.album}'
+                    f.rename(name)
+                    os.renames(f.path, os.path.join(dst_dir, f.tag.album_artist, f.tag.album, name + '.mp3'))
+                else:
+                    print(f'Warning: artist or album not defined for {f.path}')
 
 
 if __name__ == '__main__':
