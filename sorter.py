2c8e9af345f604081ce2fdee8506eabdac6d6777
Tim <jasper7466@yandex.ru>
2020-05-03 01:45:17 +0500

    Парсинг тегов. Реализована проверка версии тега - в обычном режиме работа ведётся только с тегами ID3v2+ - при запуске с параметром `--all_ver True` будут анализироваться все версии

--- sorter.py
+++ sorter.py
@@ -40,21 +40,24 @@ def allowed_x(path, *args, notify=True):    # Проверка прав дост
     return True
 
 
-def mp3_processor(path):
+def mp3_processor(path, all=False):
     """
     Function `mp3_processor(path).`
     Analyzes ID3 tags of file from `path` and generate new path according some rules.
     """
     f = mp3.load(path)  # Файл
     t = f.tag           # Тег
+    result = path       # По умолчанию как результат возвращаем исходный путь
+    if not all:
+        if t.version[0] < 2:
+            print(Fore.YELLOW + f'Предупреждение: Недопустимая версия тега ID3 для {path}')
+            return result
     if t.album_artist and t.album:  # Если оба тега не пусты, формируем новый путь
         result = f'{t.title if t.title else os.path.basename(path)[:-4]} - {t.album_artist} - {t.album + ".mp3"}'
         result = os.path.join(t.album_artist, t.album, result)
     else:                           # Если хотя бы 1 пуст - выводим сообщение
-        print(Fore.RED + f'Warning: Tags "artist" or "album" not defined for {path}')
-        result = path               # Как результат вернём исходный путь
+        print(Fore.YELLOW + f'Предупреждение: Теги "исполнитель" или "альбом" не определены для {path}')
     return result
-
 
 def move(src, dst, notify=True):
     """
@@ -71,7 +74,7 @@ def move(src, dst, notify=True):
             os.removedirs(d)        # Если файлов нет - удаляем
     finally:
         if notify:                                  # Если вызов с флагом "notify", то
-            print(Fore.GREEN + f'{src} -> {dst}')   # выводим сообщение в лог
+            print(Fore.GREEN + f'{src} -> {dst};')   # выводим сообщение в лог
 
 
 @click.command()
@@ -79,16 +82,26 @@ def move(src, dst, notify=True):
 @click.option('-d', '--dst_dir', default=os.getcwd(), help='Destination directory')
 @click.option('-n', '--nested', default=False, help='Searching nested files inside subdirectories. Set "True" to enable')
 @click.option('-c', '--create', default=False, help='Auto create destination directory. Set "True" to enable ')
-def sort(src_dir, dst_dir, nested=False, create=False):
-    """Simple program that sorts mp3-files."""
+@click.option('-a', '--all_ver', default=False, help='Analyze all ID3 versions (not only V2+). Set "True" to enable ')
+def sort(src_dir, dst_dir, nested=False, create=False, all_ver=False):
+    """
+    Simple program that sorts mp3-files.
+    Analyzes files ID3-tags data and moves them from `src_dir` into new directories, sorted by `artist` and `album` like:
+
+    <dst_dir>/<Artist_1>/<Album_1>/Track_1 - Artist_1 - Album_1.mp3
+    <dst_dir>/<Artist_1>/<Album_1>/Track_2 - Artist_1 - Album_1.mp3
+    <dst_dir>/<Artist_1>/<Album_2>/Track_3 - Artist_1 - Album_2.mp3
+    <dst_dir>/<Artist_2>/<Album_3>/Track_4 - Artist_2 - Album_3.mp3
+    ... etc.
+    """
 
     # Предварительные проверки
     if not os.path.isdir(src_dir):                  # Проверяем исходный путь на директорию
         if not allowed_x(src_dir, 'exist'):         # Проверяем наличие исходной директории,
             return                                  # Если она не существует - выходим
         else:                                       # Если существует, но не директория
-            print(Fore.RED + f'Путь {src_dir} не является директорией')
-        return
+            print(Fore.RED + f'Путь {src_dir} не является директорией') # Выводим сообщение
+            return                                  # и выходим
 
     if not allowed_x(dst_dir, 'exist'):             # Проверяем наличие директории назначения, если её нет - то
         if not create:                              # Если без вызваны флага автосоздания директории
@@ -106,7 +119,7 @@ def sort(src_dir, dst_dir, nested=False, create=False):
         for file in files:                          # Итерируемся по файлам
             if file.endswith('.mp3'):               # Если формат - mp3
                 old = os.path.join(d, file)         # Получаем "старый" путь
-                new = os.path.join(dst_dir, mp3_processor(old))     # Формируем новый путь
+                new = os.path.join(dst_dir, mp3_processor(old, all_ver))     # Формируем новый путь
                 if new != old:                      # Проверяем на идентичность путей
                     move(old, new)                  # Перемещаем файл, если они различны
         if not nested:                              # Если запустились в режиме "поверхностного поиска" - выходим,
@@ -117,3 +130,4 @@ if __name__ == '__main__':
     init(autoreset=True)        # Инициализация с автосбросом цвета текста на значение по умолчанию после вызова print()
     mp3.log.setLevel("ERROR")   # Отключаем вывод предупреждений. Теперь в консоль будут выводиться только ошибки.
     sort()                      # Запуск основной функции
+    print(Fore.BLUE + 'Done.')
