d1ed49c8666e2be2897d3e12a5dd83e2d0c4db2e
Tim <jasper7466@yandex.ru>
2020-05-02 20:42:46 +0500

    Рефакторинг. Копирование файлов вынесено в функцию move(src, dst)

--- sorter.py
+++ sorter.py
@@ -46,36 +46,42 @@ def mp3_processor(path):
     return result
 
 
+def move(src, dst, notify=True):
+    try:
+        os.renames(src, dst)        # Пытаемся переместить файл
+    except FileExistsError:         # Если в директории назначения он уже существует
+        os.replace(src, dst)        # Делаем замену
+        d = os.path.dirname(src)    # Получаем путь к папке
+        if not os.listdir(d):       # Проверяем на "пустоту", т.к. метод replace не удаляет за собой пустые папки
+            os.removedirs(d)        # Если файлов нет - удаляем
+    finally:
+        if notify:
+            print(f'{src} -> {dst}')    # Выводим сообщение в лог
+
 @click.command()
 @click.option('-s', '--src_dir', default=os.getcwd(), help='Source directory')
 @click.option('-d', '--dst_dir', default=os.getcwd(), help='Destination directory')
-@click.option('-n', '--nested', default=False, help='Set "True" for enable searching nested files inside subdirectories')
+@click.option('-n', '--nested', default=False, help='Set "True" to enable searching nested files inside subdirectories')
 def sort(src_dir, dst_dir, nested=False):
     """Simple program that sorts mp3-files."""
-
-    if not allowed_x(src_dir, 'exist'):
-        return
-    if not allowed_x(dst_dir, 'exist'):
-        if input('Хотите создать директорию [y - да / n - выход]? ') != 'y':
-            return
+    if not allowed_x(src_dir, 'exist'):             # Проверяем наличие исходной директории,
+        return                                      # если она не существует - выходим
+    if not allowed_x(dst_dir, 'exist'):             # Проверяем наличие директории назначения, если её нет - то
+        if input('Хотите создать директорию [y - да / n - выход]? ') != 'y':    # предлагаем создать
+            return                                  # Если пользователь не хочет создавать - выходим
     else:
-        if not allowed_x(dst_dir, 'write'):
-            return
+        if not allowed_x(dst_dir, 'write'):         # Если директория назначения существует,
+            return                                  # но прав на запись не имеем - выходим
 
-    for d, dirs, files in os.walk(src_dir):               # Итерирование по дереву
-        if not allowed_x(d, 'write'):
-            continue
-        for file in files:                                  # Итерировнаие по файлам
-            if file.endswith('.mp3'):                       # Если mp3
-                current = os.path.join(d, file)
-                new = os.path.join(dst_dir, mp3_processor(current))
-                try:
-                    os.renames(current, new)
-                except FileExistsError:
-                    os.replace(current, new)
-                finally:
-                    print(f'{current} -> {new}')
-        if not nested:
+    for d, dirs, files in os.walk(src_dir):         # Итерируемся по дереву
+        if not allowed_x(d, 'write'):               # Проверяем разрещение на запись в папке (чтобы переименовать файл),
+            continue                                # если его нету - идём в следующую директорию
+        for file in files:                          # Итерируемся по файлам
+            if file.endswith('.mp3'):               # Если формат - mp3
+                old = os.path.join(d, file)         # Получаем "старый" путь
+                new = os.path.join(dst_dir, mp3_processor(old))     # Формируем новый путь
+                move(old, new)                      # Перемещаем файл
+        if not nested:                              # Если запустились в режиме поверхностного поиска - выходим
             return
 
 
